e2bfd6a3880f923e64e43757ed948ce68fdb5f18
Sgtryan10 <sgtryan123456@gmail.com>
2026-01-03 21:54:48 -0500

    Timer and Quota UI and functions implemented, used quotaProgressOne() and AddTime() to change either UI display

---
+++ Assets/Scripts/Quota.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class Quota : MonoBehaviour
+{
+    [Header("UI Reference")]
+    public TextMeshProUGUI quotaText;
+
+    [Header("Settings")]
+    public int quotaGoal = 10;
+    private int quotaFulfilled = 0;
+
+    void Start()
+    {
+        UpdateDisplay();
+    }
+
+    public void quotaProgressOne(int amount = 1)
+    {
+        quotaFulfilled += amount;
+        if (quotaFulfilled > quotaGoal) quotaFulfilled = quotaGoal;
+
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        quotaText.text = $"{quotaFulfilled}/{quotaGoal}";
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float timeRemaining = 60f;
+    public float lowTimeThreshold = 30f;
+
+    private bool timerIsRunning = true;
+    private bool isFlashing = false;
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+
+                if (timeRemaining <= lowTimeThreshold && !isFlashing)
+                {
+                    StartCoroutine(FlashLowTime());
+                }
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                DisplayTime(0);
+                StopAllCoroutines();
+                timerText.color = Color.red;
+            }
+        }
+    }
+
+    public void AddTime(float secondsToAdd)
+    {
+        timeRemaining += secondsToAdd;
+
+        if (timeRemaining > lowTimeThreshold && isFlashing)
+        {
+            StopAllCoroutines();
+            isFlashing = false;
+            timerText.color = Color.white;
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    System.Collections.IEnumerator FlashLowTime()
+    {
+        isFlashing = true;
+        while (isFlashing)
+        {
+            timerText.color = Color.red;
+            yield return new WaitForSeconds(0.5f);
+
+            timerText.color = Color.white;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
